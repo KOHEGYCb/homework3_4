7a4d4b9b47ae826da3e6cc3fe13193d87f784e42
Ushenko Dmitry <dimaushenko@mail.ru>
2020-01-03 23:57:33 +0300

    refactor start services

--- src/main/java/ru/mail/dimaushenko/App.java
+++ src/main/java/ru/mail/dimaushenko/App.java
@@ -1,114 +1,19 @@
 package ru.mail.dimaushenko;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.logging.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ru.mail.dimaushenko.repository.model.Car;
-import ru.mail.dimaushenko.repository.model.CarModelEnum;
-import ru.mail.dimaushenko.service.CarService;
-import ru.mail.dimaushenko.service.CarServiceImpl;
-import ru.mail.dimaushenko.utils.PrepareDataBase;
-import ru.mail.dimaushenko.utils.RandomUtil;
+import ru.mail.dimaushenko.service.FirstTask;
+import ru.mail.dimaushenko.service.SecondTask;
+import ru.mail.dimaushenko.service.TaskService;
 
 public class App {
 
-    private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
-
-    private static final CarService CAR_SERVICE = new CarServiceImpl();
-
-    private static final String FILE_NAME = "sql";
-    private static final int AMOUNT_CARS = 10;
-    private static final int MIN_ENGINE_CAPACITY = 1;
-    private static final int MAX_ENGINE_CAPACITY = 10;
-    private static final String NEW_NAME = "Good capacity";
-
     public static void main(String[] args) {
 
-        runFirstTask();
+        TaskService first = new FirstTask();
+        first.run();
 
-        runSecondTask();
+        TaskService second = new SecondTask();
+        second.run();
 
-    }
-
-    private static void runFirstTask() {
-        File file = new File(FILE_NAME);
-        try (Scanner scanner = new Scanner(file);) {
-            while (scanner.hasNext()) {
-                String msg = scanner.nextLine();
-                LOGGER.info(msg);
-            }
-        } catch (FileNotFoundException ex) {
-            java.util.logging.Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static void runSecondTask() {
-        PrepareDataBase.prepareDB();
-        runA();
-        runB();
-        runC();
-        runD();
-        runE();
-    }
-
-    private static void runA() {
-        LOGGER.info("Start 2.a");
-
-        List<Car> cars = new ArrayList();
-        for (int i = 0; i < AMOUNT_CARS; i++) {
-            Car car = new Car();
-            car.setName("Car " + i);
-            car.setEngineCapacity(RandomUtil.getInt(MIN_ENGINE_CAPACITY, MAX_ENGINE_CAPACITY));
-            car.setCarModel(getRandCarModel());
-            cars.add(car);
-        }
-
-        CAR_SERVICE.addEntities(cars);
-    }
-
-    private static void runB() {
-        LOGGER.info("Start 2.b");
-
-        int engineCapacity = RandomUtil.getInt(MIN_ENGINE_CAPACITY, MAX_ENGINE_CAPACITY);
-        LOGGER.info("Select by EngineCapacity: " + engineCapacity);
-
-        CAR_SERVICE.getEntitiesByEngineCapacity(engineCapacity);
-
-    }
-
-    private static void runC() {
-        LOGGER.info("Start 2.c");
-        CAR_SERVICE.removeEntitiesWithMinEngineCapacity();
-    }
-
-    private static void runD() {
-        LOGGER.info("Start 2.d");
-
-        int engineCapacity = RandomUtil.getInt(MIN_ENGINE_CAPACITY, MAX_ENGINE_CAPACITY);
-        LOGGER.info("Count by EngineCapacity: " + engineCapacity);
-
-        CAR_SERVICE.getCountCarsByEngineCapacity(engineCapacity);
-
-    }
-
-    private static void runE() {
-        LOGGER.info("Start 2.e");
-
-        int engineCapacity = RandomUtil.getInt(MIN_ENGINE_CAPACITY, MAX_ENGINE_CAPACITY);
-        LOGGER.info("Change name where EngineCapacity: " + engineCapacity);
-
-        CAR_SERVICE.updateCarNameByEngineCapacity(engineCapacity, NEW_NAME);
-    }
-
-    private static CarModelEnum getRandCarModel() {
-        return CarModelEnum.values()[new Random().nextInt(CarModelEnum.values().length)];
     }
 
 }
--- src/main/java/ru/mail/dimaushenko/constants/ErrorConstants.java
+++ src/main/java/ru/mail/dimaushenko/constants/ErrorConstants.java
@@ -4,5 +4,6 @@ public class ErrorConstants {
 
     public static final String ERR_NOT_CONMECTION_TO_DB = "Can not to connect to database";
     public static final String ERR_PROP_FILE_IS_NOT_FOUND = "Config property file is not found";
+    public static final String ERR_FILE_NOT_FOUND = "File is not found";
 
 }
